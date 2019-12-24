dd109dc202606701722ff93212b55a1ca9c34064
zll <826194452@qq.com>
2019-12-24 10:48:39 +0800

    项目第七次整合

---
+++ springboot-supporter/src/main/java/com/jk/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.jk.config;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Component
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptor = registry.addInterceptor(new SessionInterceptor());
+        /**
+         * 添加拦截的路径
+         * /为根路径
+         * /*为一级路径
+         * /** 为所有路径包括多级
+         */
+
+        //放过所有请求
+        //interceptor.excludePathPatterns("/**");
+        //拦截的
+        interceptor.addPathPatterns("/to/main");
+
+
+    }
+}
